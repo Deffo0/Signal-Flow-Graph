81bac0ba6db4e551e261bca244ba48165c77f080
mahmoud@gmail.com <you@example.com>
2022-04-26 00:14:27 +0200

    calcTF

---
+++ Back-End/src/main/java/com/example/SFG/services/Tester3.java
@@ -0,0 +1,84 @@
+package com.example.SFG.services;
+
+import com.example.SFG.model.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+public class Tester3 {
+    private ForwardPathsAndLoopsGetter pathsGetter;
+
+    @Autowired
+    public Tester3(ForwardPathsAndLoopsGetter pathsGetter) {
+        this.pathsGetter = pathsGetter;
+    }
+
+    public void test() {
+        Map<Node, String> toNeighboursR = new HashMap<>();
+        Map<Node, String> toNeighbours1 = new HashMap<>();
+        Map<Node, String> toNeighbours2 = new HashMap<>();
+        Map<Node, String> toNeighbours3 = new HashMap<>();
+        Map<Node, String> toNeighbours4 = new HashMap<>();
+        Map<Node, String> toNeighbours5 = new HashMap<>();
+        Map<Node, String> toNeighbours6 = new HashMap<>();
+        Map<Node, String> toNeighboursC = new HashMap<>();
+
+        Node nodeR = new Node("R");
+        Node node1 = new Node("V1");
+        Node node2 = new Node("V2");
+        Node node3 = new Node("V3");
+        Node node4 = new Node("V4");
+        Node node5 = new Node("V5");
+        Node node6 = new Node("V6");
+        Node nodeC = new Node("C");
+
+        toNeighboursR.put(node1, "G1");
+        toNeighboursR.put(node4, "G5");
+        toNeighbours1.put(node2, "G2");
+        toNeighbours2.put(node1, "H2");
+        toNeighbours2.put(node3, "G3");
+        toNeighbours3.put(nodeC, "G4");
+        toNeighbours4.put(node5, "G6");
+        toNeighbours5.put(node4, "H6");
+        toNeighbours5.put(node6, "G7");
+        toNeighbours6.put(node5, "H7");
+        toNeighbours6.put(nodeC, "G8");
+
+        nodeR.setToNeighbours(toNeighboursR);
+        node1.setToNeighbours(toNeighbours1);
+        node2.setToNeighbours(toNeighbours2);
+        node3.setToNeighbours(toNeighbours3);
+        node4.setToNeighbours(toNeighbours4);
+        node5.setToNeighbours(toNeighbours5);
+        node6.setToNeighbours(toNeighbours6);
+        nodeC.setToNeighbours(toNeighboursC);
+
+        List<Node> vertices = new ArrayList<>();
+
+        vertices.add(nodeR);
+        vertices.add(node1);
+        vertices.add(node2);
+        vertices.add(node3);
+        vertices.add(node4);
+        vertices.add(node5);
+        vertices.add(node6);
+        vertices.add(nodeC);
+
+        pathsGetter.setVertices(vertices);
+        pathsGetter.setSourceIndex(0);
+        pathsGetter.setSinkIndex(vertices.size() - 1);
+        pathsGetter.getPaths();
+        var x = pathsGetter.getNonTouchingLoops();
+        try {
+            var y = pathsGetter.calcDelta();
+            System.out.println(y);
+        }catch (Exception e){
+            System.out.println("Error in calcDelta");
+        }
+
+
+        System.out.println(pathsGetter.getNonTouchingLoops());
+
+    }
+
+}
